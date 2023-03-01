6973327cf9d82cdb8325862d75ea4e5105ed7cd1
JenniferCV19 <125518075+JenniferCV19@users.noreply.github.com>
2023-02-28 20:05:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//variable
+let contador = 0;
+const parrafo = document.getElementById('demo');
+const boton_dis = document.getElementById('bt-1');
+const boton_rei = document.getElementById('bt-2');
+const boton_aum = document.getElementById('bt-3');
+
+//eventos de accion por boton
+boton_dis.addEventListener('click', contador_dis);
+boton_rei.addEventListener('click', contador_rei);
+boton_aum.addEventListener('click', contador_aum);
+
+//declaracion funciones
+
+function contador_dis(){
+    --contador
+    colorContador(contador)
+    return(parrafo.innerText = contador);
+}
+function contador_rei(){
+    contador = 0;
+    colorContador(contador)
+    return(parrafo.innerText = contador);
+}
+function contador_aum(){
+    ++contador
+    colorContador(contador)
+    return(parrafo.innerText = contador);
+}
+
+function colorContador(value){
+    let resultado;
+    if(value >0){
+      resultado = parrafo.style.color = 'green'
+    }
+    if(value ==0){
+        resultado = parrafo.style.color = 'black'
+    }
+    if(value <0){
+        resultado = parrafo.style.color = 'red'
+    }
+    return resultado
+
+}
